79ee1521d3a08639471e29237766444d06b65c2c
yash825-bit <goyal.yash18335@gmail.com>
2025-11-17 08:48:13 +0530

    fixed the ui of about section and created contact section

--- frontend/src/routes/MainRoutes.jsx
+++ frontend/src/routes/MainRoutes.jsx
@@ -3,37 +3,50 @@ import Home from "../pages/Home";
 import About from "../pages/About";
 import PageNotFound from "../pages/PageNotFound";
 import Login from "../pages/Login";
+import Contact from "../pages/Contact";
 import Signup from "../pages/Signup";
 import AdminDashboard from "../pages/admin/AdminDashboard";
 import AdminUsers from "../pages/admin/AdminUsers";
 import RecruiterDashboard from "../pages/recruiter/RecruiterDashboard";
 import CandidateDashboard from "../pages/candidate/CandidateDashboard";
 
-export default function MainRoutes()
-{
+export default function MainRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
 
-        <Route path="/signup/*" element={<Navigate to="/signup/candidate" replace />} />
+        <Route
+          path="/signup/*"
+          element={<Navigate to="/signup/candidate" replace />}
+        />
         <Route path="/signup/:role" element={<Signup />} />
 
         <Route path="/login" element={<Login />} />
 
-        <Route path="/admin" element={<Navigate to="/admin/dashboard" replace />} />
+        <Route
+          path="/admin"
+          element={<Navigate to="/admin/dashboard" replace />}
+        />
         <Route path="/admin/dashboard" element={<AdminDashboard />} />
         <Route path="/admin/dashboard/users" element={<AdminUsers />} />
 
-        <Route path="/recruiter" element={<Navigate to="/recruiter/dashboard" replace />} />
+        <Route
+          path="/recruiter"
+          element={<Navigate to="/recruiter/dashboard" replace />}
+        />
         <Route path="/recruiter/dashboard" element={<RecruiterDashboard />} />
 
-        <Route path="/candidate" element={<Navigate to="/candidate/dashboard" replace />} />
+        <Route
+          path="/candidate"
+          element={<Navigate to="/candidate/dashboard" replace />}
+        />
         <Route path="/candidate/dashboard" element={<CandidateDashboard />} />
 
-        <Route path="*" element={<PageNotFound />} />
+        {/* <Route path="*" element={<PageNotFound />} /> */}
       </Routes>
     </BrowserRouter>
   );
